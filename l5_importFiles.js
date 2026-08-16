0d31ee2f7d07cdc61ab594a4acdc796827a8aef6
karan <shahkaran0675@gmail.com>
2026-08-16 18:54:06 +0530

    node done

---
+++ l5_importFiles.js
@@ -0,0 +1,6 @@
+//to import files you need to create type = module in package.json and for that use npm init
+// import {sum,mul} from "./l5_import"
+import { generate } from "random-words";
+// console.log(sum(3,4));
+console.log(generate());
+
